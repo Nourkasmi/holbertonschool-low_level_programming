c7e3a80051fc1d2356140093f0c0369d9125c1b0
NourKasmi <nourkasmi@gmail.com>
2024-10-24 10:44:37 +0100

    2-print_alphabet.c

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 char n;
@@ -8,5 +12,5 @@ for (n = 'a'; n <= 'z'; n++)
 putchar(n);
 }
 putchar('\n');
-return (0);
+return (1);
 }
