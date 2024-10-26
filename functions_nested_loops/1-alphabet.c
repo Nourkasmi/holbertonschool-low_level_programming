a8b29be7adc22093e1560bb9de127a5e3b47dc8c
NourKasmi <nourkasmi@gmail.com>
2024-10-26 13:00:02 +0100

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+char alphabet[] = "abcdefghijklmnopqrstuvwxyz"; 
+int i;
+
+for (i = 0; i < 26; i++)
+{
+_putchar(alphabet[i]); 
+}
+_putchar('\n'); 
+}
