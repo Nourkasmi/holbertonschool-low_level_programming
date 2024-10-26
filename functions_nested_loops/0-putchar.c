4bc3bc59fc084647def6e0932f66a9949d52c822
NourKasmi <nourkasmi@gmail.com>
2024-10-26 11:51:28 +0100

    0-putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char msg[] = "_putchar\n";
+write(1, msg, sizeof(msg) - 1);
+return (0);
+}
