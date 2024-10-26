ab5b8adde523252a2707db8026333b0b1a699828
NourKasmi <nourkasmi@gmail.com>
2024-10-26 13:02:07 +0100

    1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -8,12 +8,12 @@
  */
 void print_alphabet(void)
 {
-char alphabet[] = "abcdefghijklmnopqrstuvwxyz"; 
+char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
 int i;
 
 for (i = 0; i < 26; i++)
 {
-_putchar(alphabet[i]); 
+_putchar(alphabet[i]);
 }
-_putchar('\n'); 
+_putchar('\n');
 }
