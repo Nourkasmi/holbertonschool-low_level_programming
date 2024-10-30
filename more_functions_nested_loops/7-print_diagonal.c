aba71683530dc7b649aaa18b7996ea9f6b3b3c83
NourKasmi <nourkasmi@gmail.com>
2024-10-30 12:39:24 +0100

    8-print_square

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -2,13 +2,6 @@
 /**
  * print_diagonal - Draws a diagonal line on the terminal.
  * @n: The number of times the '\' character is printed to form the line.
- *
- * Description: This function prints a diagonal line on the terminal.
- * For each line, it offsets the '\' character by an increasing number
- * of spaces to create the diagonal effect. The number of lines in the
- * diagonal is determined by `n`. If `n` is 0 or less, the function
- * simply prints a newline. Each line of the diagonal ends with a newline.
- *
  * Return: void
  */
 
