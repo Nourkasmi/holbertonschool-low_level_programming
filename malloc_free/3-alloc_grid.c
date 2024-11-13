3923c3e1bb07878de42b6e7a338dd7d7b2ac38bd
NourKasmi <nourkasmi@gmail.com>
2024-11-13 10:54:46 +0100

    3-alloc_grid.c

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -1,34 +1,42 @@
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * alloc_grid - Allocates a 2D array of integers and initializes elements to 0.
- * @w: Width of the grid (number of columns).
- * @h: Height of the grid (number of rows).
- *
- * Return: Pointer to the allocated grid or NULL on failure.
+ * alloc_grid - creates a 2-dimensional.
+ * @width: the width of the grid.
+ * @height: the height of the grid.
+ * Return: pointer to the newly allocated grid.
+ * or if width or height is 0 or negative
  */
-int **alloc_grid(int w, int h)
+int **alloc_grid(int width, int height)
 {
-int **matrice;
-int i;
-if (w <= 0 || h <= 0)
-return (NULL);
-matrice = malloc(h * sizeof(int *));
-if (matrice == NULL)
-return (NULL);
-for (i = 0; i < h; i++)
+int **grid;
+int i, j;
+if (width <= 0 || height <= 0)
 {
-matrice[i] = calloc(w, sizeof(int));
-if (matrice[i] == NULL)
-{
-while (i >= 0)
-{
-free(matrice[i]);
-i--;
-}
-free(matrice);
 return (NULL);
 }
+grid = (int **)malloc(height * sizeof(int *));
+if (grid == NULL)
+{
+return (NULL);
 }
-return (matrice);
+for (i = 0; i < height; i++)
+{
+grid[i] = (int *)malloc(width * sizeof(int));
+if (grid[i] == NULL)
+{
+for (j = 0; j < i; j++)
+{
+free(grid[j]);
+}
+free(grid);
+return (NULL);
+}
+for (j = 0; j < width; j++)
+{
+grid[i][j] = 0;
+}
+}
+return (grid);
 }
