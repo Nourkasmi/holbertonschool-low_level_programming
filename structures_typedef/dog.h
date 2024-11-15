d16dada3970a9a04eaff8289e6f4e9d1c9d85b18
NourKasmi <nourkasmi@gmail.com>
2024-11-15 12:14:12 +0100

    woof

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Represents details of a dog
+ * @name: The name of the dog of type: char *.
+ * @age: The age of the dog of type: float.
+ * @owner: The name of the dog's owner of type: char *.
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
