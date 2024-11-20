6aa006cb408df9b9f4aa270df8af31f5360da6a9
NourKasmi <nourkasmi@gmail.com>
2024-11-20 13:26:38 +0100

    0-print_name.c

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,16 +1,15 @@
 #include "function_pointers.h"
-#include <stddef.h>
 
 /**
  * print_name-function that prints a name
  * @f:function
  * @name:the name
  */
-
+ 
 void print_name(char *name, void (*f)(char *))
 {
-if (name == NULL || f == NULL)
-return;
+if (name && f)
+{
 f(name);
 }
-
+}
