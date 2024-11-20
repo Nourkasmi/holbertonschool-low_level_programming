590bfb28960250f695b46484d274b7e12cfa5d3d
NourKasmi <nourkasmi@gmail.com>
2024-11-20 12:56:11 +0100

    0-print_name.c

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,8 +1,7 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#include <stddef.h>
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *)); 
