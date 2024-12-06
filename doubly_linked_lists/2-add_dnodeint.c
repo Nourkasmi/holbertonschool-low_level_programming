4f37a2c7081c3542fee93b8693b95a699fd62df6
NourKasmi <nourkasmi@gmail.com>
2024-12-06 12:43:06 +0100

    2-add_dnodeint.c

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - check the code
+ * @head: list
+ * @n: int
+ * Return: Always 0.
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+dlistint_t *new_node
+new_node = malloc(sizeof(dlistint_t));
+if (!new_node || !head)
+{
+free(new_node);
+return (NULL);
+}
+}
