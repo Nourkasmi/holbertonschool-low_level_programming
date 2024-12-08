fa61b35c89a54ef945789ddcee6866798e3c52d1
NourKasmi <nourkasmi@gmail.com>
2024-12-08 14:11:37 +0100

    testing number 0

---
+++ bit_manipulation/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+
+unsigned int binary_to_uint(const char *b);
+int _putchar(char c);
+
+#endif
