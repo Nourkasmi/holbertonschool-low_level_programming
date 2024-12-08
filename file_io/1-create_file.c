9ad80850f49cd8dd697ccc51ad6710b6b29dfae2
NourKasmi <nourkasmi@gmail.com>
2024-12-08 17:36:10 +0100

    1-create_file.c

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,33 +1,22 @@
 #include "main.h"
-
 /**
- * create_file - Creates a file with specified content.
- * @filename: The name of the file to create.
- * @text_content: A NULL-terminated string to write to the file.
+ * create_file - a function that creates a file.
+ * @filename : the name of the file
+ * @text_content: the content of the file
  *
  * Return: 1 on success, -1 on failure.
  */
 int create_file(const char *filename, char *text_content)
 {
-int fd;
-ssize_t bytes_written;
-int len = 0;
+int length = 0, file;
 if (filename == NULL)
 return (-1);
-fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-if (fd == -1)
+if (text_content == NULL)
+text_content = "\0";
+length = strlen(text_content);
+file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+if (file == -1)
 return (-1);
-if (text_content != NULL)
-{
-while (text_content[len])
-len++;
-bytes_written = write(fd, text_content, len);
-if (bytes_written == -1)
-{
-close(fd);
-return (-1);
-}
-}
-close(fd);
+write(file, text_content, length);
 return (1);
 }
